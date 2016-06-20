f7d218ca70387dca90ff42e271462ac8918b42ac
gokemon <michael.of.gokey@gmail.com>
2016-06-20 18:04:08 -0400

    Michael Gokey Resume

---
+++ polyfill.js
@@ -0,0 +1,5 @@
+(function(window, document) {
+  const elements = ['main', 'header', 'section', 'details', 'summary', 'time', 'footer'];
+  for (var i = 0; i < elements.length; i++) { 
+  	document.createElement(elements[i]); 
+  } })(window, document);
